2bae71b9b434c8ede7dc9e392cb47a2838507386
Marco Brugin <marcobrugin00@gmail.com>
2023-04-19 19:32:58 +0200

    Refactoring

--- src/main/java/com/example/Trustifyrest/Review_Request.java
+++ src/main/java/com/example/Trustifyrest/Review_Request.java
@@ -1,31 +1,5 @@
 package com.example.Trustifyrest;
 
-public class Review_Request {
-    private String companyAddress;
-    private int startRange, endRange;
-    private static final int maxreviews =25;
-    public Review_Request(String address, int startRange, int endRange) {
-        if(startRange < 0)
-            throw new IllegalArgumentException("Error: startRange can not be negative");
-        if(startRange > endRange)
-            throw new IllegalArgumentException("Error: startRange can not be grater that endRange");
-        if(!org.web3j.crypto.WalletUtils.isValidAddress(companyAddress)){
-            throw new IllegalArgumentException("Error: not a valid address");
-        }
-        if((endRange - startRange) > maxreviews){
-            throw new IllegalArgumentException("Error: you can't ask for more than 25 review per call");
-        }
-    }
+public class Checker {
 
-    public String getCompanyAddress() {
-        return companyAddress;
-    }
-
-    public int getStartRange() {
-        return startRange;
-    }
-
-    public int getEndRange() {
-        return endRange;
-    }
 }
