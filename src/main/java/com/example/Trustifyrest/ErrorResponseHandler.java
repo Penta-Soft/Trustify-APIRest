c55986710b256a1a519d316533d5f09b22715f0a
Marco Brugin <marcobrugin00@gmail.com>
2023-04-22 12:28:06 +0200

    FIx API error handler

--- src/main/java/com/example/Trustifyrest/ErrorResponseHandler.java
+++ src/main/java/com/example/Trustifyrest/ErrorResponseHandler.java
@@ -2,15 +2,24 @@ package com.example.Trustifyrest;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ErrorResponseHandler extends ResponseEntityExceptionHandler {
+public class ErrorResponseHandler extends ResponseEntityExceptionHandler  {
 
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        headers = new HttpHeaders();
+        headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(new ResponseStatusException(HttpStatus.BAD_REQUEST,ex.getMessage()).getMessage(), headers, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Object> handleAnyException(ResponseStatusException ex){
         HttpHeaders headers = new HttpHeaders();
@@ -22,5 +31,6 @@ public class ErrorResponseHandler extends ResponseEntityExceptionHandler {
         else
             return new ResponseEntity<>(ex.getMessage(), headers, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 
 }
--- src/main/java/com/example/Trustifyrest/SmartContractReaderController.java
+++ src/main/java/com/example/Trustifyrest/SmartContractReaderController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.web3j.tx.exceptions.ContractCallException;
 
 import java.net.ConnectException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -14,24 +16,29 @@ import java.util.concurrent.ExecutionException;
 public class SmartContractReaderController {
 
     @GetMapping("/reviews")
-    public List<Review> SmartContractReader(
-            @RequestParam(value = "address", defaultValue = "") String address,
+
+    public List<Review> SmartContractReaderController(
+            @RequestParam(value = "address") String address,
             @RequestParam(value = "startRange") int startRange,
             @RequestParam(value = "endRange") int endRange) throws Exception {
         TrustifyContractReader trustify = new TrustifyContractReader(new Review_Request(address, startRange, endRange));
+        List<Review> l = new ArrayList<>();
         try {
-            return trustify.getReviews();
+            l=trustify.getReviews();
+
         } catch (ExecutionException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error: contract not found");
-        } catch (ConnectException e) {
-            System.out.println(e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error: connection failed");
+            if(e.getCause() instanceof ContractCallException)
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error: This company have not received any reviews yet");
+            else
+                if(e.getCause() instanceof  IndexOutOfBoundsException)
+                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error: Contract is not deployed yet");
         }
-        catch (Exception e){
-            System.out.println(e);
+        catch (ConnectException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error: connection failed");
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error: unknown error");
         }
-
+    return l;
     }
 
 
