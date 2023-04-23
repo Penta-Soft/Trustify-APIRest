4420d39e588bcc8eda76c55dd2704253e6797894
lazza <nicola.lazzarin89@gmail.com>
2023-04-23 13:07:44 +0200

    added test

--- src/test/java/com/example/Trustifyrest/TrustifyRestApplicationTests.java
+++ src/test/java/com/example/Trustifyrest/TrustifyRestApplicationTests.java
@@ -36,24 +36,32 @@ class TrustifyRestApplicationTests {
 
 	@Test
 	void Insert_a_non_valid_address() throws Exception {
-		final String companyAddress = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EE3595D58&startRange=0&endRange=4";
-		RequestBuilder request = MockMvcRequestBuilders.get(companyAddress);
+		final String endpoint = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EE3595D58&startRange=0&endRange=4";
+		RequestBuilder request = MockMvcRequestBuilders.get(endpoint);
 		MvcResult result = mockMvc.perform(request).andReturn();
 		assertEquals("400 BAD_REQUEST \"Error: address is not valid\"",result.getResponse().getContentAsString());
 	}
 
 	@Test
+	void Ask_for_a_non_existing_endpoint() throws Exception{
+		final String endpoint = "/wrongEndpoint";
+		RequestBuilder request = MockMvcRequestBuilders.get(endpoint);
+		MvcResult result = mockMvc.perform(request).andReturn();
+		assertEquals("404 NOT_FOUND \"Error: the requested resource was not found\"",result.getResponse().getContentAsString());
+	}
+
+	@Test
 	void Insert_a_non_valid_startRange() throws Exception{
-		final String companyAddress = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=-1&endRange=4";
-		RequestBuilder request = MockMvcRequestBuilders.get(companyAddress);
+		final String endpoint = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=-1&endRange=4";
+		RequestBuilder request = MockMvcRequestBuilders.get(endpoint);
 		MvcResult result = mockMvc.perform(request).andReturn();
 		assertEquals("400 BAD_REQUEST \"Error: startRange can not be negative\"",result.getResponse().getContentAsString());
 	}
 
 	@Test
 	void Insert_startRanege_grater_than_endRange() throws Exception{
-		final String companyAddress = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=5&endRange=4";
-		RequestBuilder request = MockMvcRequestBuilders.get(companyAddress);
+		final String endpoint = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=5&endRange=4";
+		RequestBuilder request = MockMvcRequestBuilders.get(endpoint);
 		MvcResult result = mockMvc.perform(request).andReturn();
 		assertEquals("400 BAD_REQUEST \"Error: startRange can not be grater that endRange\"",result.getResponse().getContentAsString());
 	}
@@ -70,16 +78,16 @@ class TrustifyRestApplicationTests {
 
 	@Test
 	void Call_for_a_company_without_any_reviews () throws Exception{
-		final String companyAddress = "/reviews?address=0x20DcB1C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=0&endRange=4";
-		RequestBuilder request = MockMvcRequestBuilders.get(companyAddress);
+		final String endpoint = "/reviews?address=0x20DcB1C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=0&endRange=4";
+		RequestBuilder request = MockMvcRequestBuilders.get(endpoint);
 		MvcResult result = mockMvc.perform(request).andReturn();
 		assertEquals("400 BAD_REQUEST \"Error: This company have not received any reviews yet\"",result.getResponse().getContentAsString());
 	}
 
 	@Test
 	void Ask_for_more_than_25_review_per_call() throws Exception{
-		final String companyAddress = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=0&endRange=30";
-		RequestBuilder request = MockMvcRequestBuilders.get(companyAddress);
+		final String endpoint = "/reviews?address=0x20DcB8C5c4C4891DeF4B3f0D8BC2C3EEE3595D58&startRange=0&endRange=30";
+		RequestBuilder request = MockMvcRequestBuilders.get(endpoint);
 		MvcResult result = mockMvc.perform(request).andReturn();
 		assertEquals("400 BAD_REQUEST \"Error: you can't ask for more than 25 review per call\"",result.getResponse().getContentAsString());
 	}
